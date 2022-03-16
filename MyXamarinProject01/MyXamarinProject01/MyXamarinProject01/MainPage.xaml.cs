72fd164f6f28860c80863fea8baf7c937862ef52
Raul Armas <you@example.com>
2022-03-16 14:24:03 -0500

    first commit SMA

---
+++ MyXamarinProject01/MyXamarinProject01/MyXamarinProject01/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyXamarinProject01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MenuPage());
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
